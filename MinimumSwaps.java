21e540365aad3d0b21a9c62ff6b573a322b07412
Abhishek Yelisetti <abhishekyelisetti56050@gmail.com>
2020-09-27 08:37:12 +0530

    Elite day 124

--- MinimumSwaps.java
+++ MinimumSwaps.java
@@ -66,8 +66,7 @@ class MinimumSwaps {
         return Math.min(n1, s1);
     }
 	
-	public static void main(String args[])
-	{
+	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int A[]=new int[n];
